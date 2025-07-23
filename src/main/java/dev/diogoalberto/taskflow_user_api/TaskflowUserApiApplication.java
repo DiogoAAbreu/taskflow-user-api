130a60838c1263b4d85347bb7c53bd45636d2ea7
Diogo Alberto <diogoalberto.dev@gmail.com>
2025-07-23 04:43:37 -0300

    initial commit

---
+++ src/main/java/dev/diogoalberto/taskflow_user_api/TaskflowUserApiApplication.java
@@ -0,0 +1,13 @@
+package dev.diogoalberto.taskflow_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/diogoalberto/taskflow_user_api/TaskflowUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diogoalberto.taskflow_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
